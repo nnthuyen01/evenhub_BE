7c654f6c6967e05a0c682895b11ff43164dd9397
Thuyen Nguyen Ngoc <tmdthuyen@gmail.com>
2024-02-05 15:47:44 +0700

    login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -44,6 +44,31 @@ const register = asyncHandler(async (req, res) => {
   });
 });
 
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await UserModel.findOne({ email });
+  if (!existingUser) {
+    res.status(403).json({
+      message: 'User not found!!!',
+    });
+    throw new Error('User not found!!!');
+  }
+  const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isMatchPassword) {
+    res.status(401);
+    throw new Error('Email or password is not correct!!!');
+  }
+  res.status(200).json({
+    message: 'Login successfully',
+    data: {
+      id: existingUser.id,
+      email: existingUser.email,
+      accessToken: await getJsonWebToken(email, existingUser.id),
+    },
+  });
+});
+
 module.exports = {
   register,
+  login,
 };
